75612c4820c6b54390d00768ac2b036ce6825fc6
Kristopher Navarro <kristopher.navarro@icloud.com>
2023-04-10 11:57:39 -0500

    Updated README and locations
    - The README now has a better explanation of the purpose of the repository. AMENDMENT: edited text and links in README
    - New directory created: Class Project - Text Based Game.
    - All project files moved to Class Project dir.
    - NameAge.py moved to Assignments dir.

---
+++ Class Project - Text Based Game/TextBasedGame.py
@@ -0,0 +1,83 @@
+# Created by: Kristopher Navarro
+
+#A dictionary for the simplified dragon text game
+#The dictionary links a room to other rooms.
+map = {
+        'Great Hall': {'South': 'Bedroom'},
+        'Bedroom': {'North': 'Great Hall', 'East': 'Cellar'},
+        'Cellar': {'West': 'Bedroom'}
+    }
+
+items = {
+        'Great Hall': {
+            'item': 'Torch',
+            'observed': False },
+        'Bedroom': {
+            'item': 'Pillow',
+            'observed': False },
+        'Cellar': {
+            'item': 'Axe',
+            'observed': False }
+}
+movement_verbs = ('Go', 'Move', 'Head')
+action_verbs = ('Get', 'Take', 'Grab')
+obs_verbs = ('Look', 'See', 'Examine')
+directions = ('North', 'South', 'East', 'West')
+
+current_room = 'Great Hall'
+inventory = {
+        'slot 1': False,
+        'slot 2': False,
+        'slot 3': False,
+}
+
+print('''
+Welcome to the Mansion!
+''')
+
+while True:
+    user_input = str(input('What would you like to do? \n')).title()
+    tokens = user_input.split()
+    for token in tokens:
+        if token in movement_verbs:
+            for token in tokens:
+                if token in directions:
+                    try:
+                        current_room = map[current_room][token]
+                        print(f'You are now in the {current_room}.')
+                    except KeyError:
+                        print('Only ghosts can walk through walls, try again.')
+            continue
+        elif token in obs_verbs:
+            if items[current_room]['item'] == False:
+                print('There is nothing here.')
+            else:
+                items[current_room]['observed'] = True
+                print(f'You see a {items[current_room]["item"]}')
+            continue
+        elif token in action_verbs:
+            if items[current_room]['observed'] == False:
+                print('There is nothing to take, try looking around.')
+                continue
+            else:
+                new_inventory = items[current_room]['item']
+                for k,v in inventory.items():
+                    if v == False:
+                        inventory[k] = items[current_room]['item']
+                        items[current_room]['item'] = False
+                print(f'You now have the {new_inventory}.')
+            continue
+
+        elif token == 'Inventory':
+            for k,v in inventory.items():
+                if v != False:
+                    print(f'{k} : {v}')
+                else:
+                    print(f'{k} :     ')
+            continue
+        #print('That was an invalid command, try again.')
+    if 'Quit' in tokens:
+        break
+
+print('Thanks for playing!')
+
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# snhu-it140
- Project files for class IT-140
+![Southern New Hampshire University](https://images.squarespace-cdn.com/content/v1/5bec669f9f877016e7212b2d/c4d5c253-8a2d-4c31-bf85-33cbb7fd6ca3/snhu_wordmark_rgb_pos.png?format=300w)
+
+<i>Class:</i> <b>IT-140</b> </p>
+<i>Term:</i> <b>23EW4</b>
+
+Welcome! </p>
+This repository is a general collection of all files related to my classwork for SNHU class IT-140: Introduction to Scripting. 
+Any files related to zyBooks prompts are appropriately named with corresponding section numbers, these can be found in the [zyBooks Labs](https://github.com/KristopherNavarro/snhu-it140/settings/branch_protection_rules/new?branch_name=main) directory.
+All files pertaining to the final class project can be found in the [Class Project - Text Based Game](https://github.com/KristopherNavarro/snhu-it140/settings/branch_protection_rules/new?branch_name=main) directory.
+This includes a Jupyter Notebook where I collected and tested ideas for the final project, as well as the final project files.
+
+I welcome and encourage any feedback or questions, thank you!
+
+Cheers, </p>
+Kristopher Navarro
